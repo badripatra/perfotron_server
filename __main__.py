5cd72f0f4d725d3644d35b9542cbcfdef13cc4ca
bpatra <badriprasad.patra@netapp.com>
2019-10-31 14:48:29 +0530

    Shell Script code fixes for influx - 19

--- __main__.py
+++ __main__.py
@@ -27,10 +27,11 @@ def dependency_resolution():
     """ This function is responsible to install dependent packages """
 
     print "-----------------Resolving Dependencies--------------------------"
-    file_name = os.path.join(CURRENT_DIR, "os_specific_files", OS_TYPE, "install_dependency.sh")
-    log_file_name = os.path.join(HOME, "installation_launchpad", "Installation_details.log")
-    command = file_name + " >> " + log_file_name + " 2>&1"
-    os.system(command)
+    dependency_details = os.path.join(CURRENT_DIR, "os_specific_files", OS_TYPE,
+                                      "install_dependency.sh")
+    cmd = dependency_details + " >/dev/null 2>&1"
+    print cmd
+    os.system(cmd)
 
     print "1. pip"
     print "2. jdk (8)"
@@ -308,28 +309,6 @@ if __name__ == '__main__':
         sys.exit()
 
     OS_TYPE = get_command_output("python -mplatform")
-
-    print "-----------------Resolving Dependencies--------------------------"
-    file_name = os.path.join(CURRENT_DIR, "os_specific_files", OS_TYPE, "install_dependency.sh")
-    print "1"
-    print file_name
-    log_file_name = os.path.join(HOME, "installation_launchpad", "Installation_details.log")
-    print "2"
-    print log_file_name
-    command = file_name + " >> " + log_file_name + " 2>&1"
-    print command
-    os.system(command)
-
-    print "1. pip"
-    print "2. jdk (8)"
-    print "3. flask (python module)"
-    print "4. inquirer (python module)"
-    print "5. influxdb (python module)"
-    print "6. selenium (python module)"
-    print "7. pyvirtualdisplay (python module)"
-    print "\n"
-
-    print "-----------------Resolving Dependencies--------------------------"
 
     if "Ubuntu" in OS_TYPE:
         OS_TYPE = "ubuntu"
