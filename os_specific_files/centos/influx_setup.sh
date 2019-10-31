d115b02048b989b7d44660045493cd9c796ccd5d
bpatra <badriprasad.patra@netapp.com>
2019-10-31 08:34:58 +0530

    Shell Script code fixes for influx - 2

--- os_specific_files/centos/influx_setup.sh
+++ os_specific_files/centos/influx_setup.sh
@@ -12,14 +12,13 @@ gpgkey = https://repos.influxdata.com/influxdb.key
 EOF
 sudo yum -y update
 sudo yum -y install influxdb
-sudo cp influxdb.conf /etc/influxdb/influxdb.conf
 sudo service influxdb start
 sudo influxd -config /etc/influxdb/influxdb.conf
-sleep 5
+sudo sed -i -e 's/  # Determines whether HTTP endpoint is enabled.\n  # enabled = true/  # Determines whether HTTP endpoint is enabled.\n  enabled = true/g' /etc/influxdb/influxdb.conf
+sudo sed -i -e 's/auth-enabled = false/auth-enabled = true/g' /etc/influxdb/influxdb.conf
 sudo curl -G "http://localhost:8086/query" --data-urlencode "q=CREATE DATABASE jmeter"
 sudo curl -G "http://localhost:8086/query" --data-urlencode "q=CREATE USER lnp_automation WITH PASSWORD 'lnp_automation' WITH ALL PRIVILEGES"
 sudo service influxdb restart
-sudo yum -y install python-pip
 else
     echo "Skipping Step : Influx Setup as as installer identified a existing Influx DB Setup. "
 fi
