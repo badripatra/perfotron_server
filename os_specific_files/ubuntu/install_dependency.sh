134131ab2efd9d2afab18d392ab3e905874f8b62
bpatra <badriprasad.patra@netapp.com>
2019-11-03 21:25:10 +0530

    Dependency resolution Code Fix

--- os_specific_files/ubuntu/install_dependency.sh
+++ os_specific_files/ubuntu/install_dependency.sh
@@ -1,4 +1,4 @@
-sudo apt-get -y install python-pip -qqq
 sudo apt-get update -qqq
+sudo apt-get -y install python-pip -qqq
 sudo apt-get -y install openjdk-8-jdk -qqq
 sudo pip -q install -r requirements.txt
