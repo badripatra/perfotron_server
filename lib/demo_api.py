ab153713b3f8d0388e0dfb1abefc384a47a5a803
bpatra <badriprasad.patra@netapp.com>
2019-11-26 17:07:16 +0530

    Added few rest methods for converting jmx online -3

--- lib/demo_api.py
+++ lib/demo_api.py
@@ -6,13 +6,99 @@ __email__ = "bpatra@netapp.com"
 __status__ = "Testing"
  === DEPLOY REST API For Perf Dashboard Demo =="""
 
+import os
 import sys
 from flask import Flask, render_template
 from flask import request
+from xml.etree import ElementTree as ET
+import socket
+import subprocess
 
 FLASK_APP = Flask(__name__)
 PORT = int("9003")
 ALLOWED_EXTENSIONS = {'jmx'}
+CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
+
+
+def identify_onprem_or_cloud():
+    """ This function is responsible to identify if a system is part of cloud provider """
+    auzure = get_command_output("sudo dmidecode|grep Microsoft|wc -l")
+    aws = get_command_output("sudo dmidecode|grep amazon|wc -l")
+    gcp=get_command_output("sudo dmidecode|grep Google|wc -l")
+
+    if int(auzure) > 0:
+        cloud_vendor = "auzure"
+    elif int(aws) > 0:
+        cloud_vendor = "aws"
+    elif int(gcp) > 0:
+        cloud_vendor = "gcp"
+    else:
+        cloud_vendor = "NA"
+    return cloud_vendor
+
+
+def get_jmxchecker_output(command):
+    """ This function is responsible for running a command on Shell & return output"""
+    cmd = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    try:
+        cmd_value = str(cmd.stdout.readlines()).strip()  # Read command output
+        sys.stdout.flush()
+    except IndexError:
+        cmd_value = ""
+
+    return cmd_value
+
+
+def get_command_output(command):
+    """ This function is responsible for running a command on Shell & return output"""
+    cmd = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    try:
+        cmd_value = str(cmd.stdout.readlines()[0]).strip()  # Read command output
+        sys.stdout.flush()
+    except IndexError:
+        cmd_value = ""
+
+    return cmd_value
+
+
+def get_ip(cloud_vendor):
+    """ This function is responsible to identify clouder provider from BIOS """
+
+    if cloud_vendor != "NA":
+        ip_address = get_command_output("curl -s ifconfig.me")
+    else:
+        hostname = socket.gethostname()
+        ip_address = socket.gethostbyname(hostname)
+
+    return ip_address
+
+
+def add_backend_listner(jmx_string):
+    """ This function is responsible for adding back end listener to a existing jmx"""
+    
+    cloud_vendor = identify_onprem_or_cloud
+    IP = get_ip(cloud_vendor)
+
+    with open('backend_listner.jmx') as backend_listner:
+        backend_listner_data = str(backend_listner.read()).replace("[IP]", IP)
+
+    with open('user_script.jmx', "w") as backend_listner:
+        backend_listner.write(jmx_string)
+
+    with open('backend_listner.jmx', "w") as backend_listner:
+        backend_listner.write(backend_listner_data)
+
+    backend_listener = ET.parse("backend_listner.jmx")
+
+    base_script = ET.parse('user_script.jmx')
+    existing_struct = base_script.find("./hashTree/hashTree")
+    new_condition = backend_listener.getroot()
+    existing_struct.append(new_condition)
+
+    with open('user_script.jmx', "w") as user_jmx:
+        user_jmx.write(ET.tostring(base_script.getroot()))
+
+
 
 
 def allowed_file(filename):
@@ -34,13 +120,16 @@ def api_message():
 
 @FLASK_APP.route('/convert_jmx', methods=['GET', 'POST'])
 def upload_file():
+
     if request.method == 'POST':
+
         file = request.files['file']
 
         if file:
             if allowed_file(file.filename):
                 content = file.read()
-                return content
+                content_modified = add_backend_listner(content)
+                return content_modified
             else:
                 return render_template('file_extension_not_allowed.html')
 
