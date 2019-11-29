1f9a14fac683122d56f3434a5ecd344f20771c83
bpatra <badriprasad.patra@netapp.com>
2019-11-29 15:57:24 +0530

    Added print statement for JMX checker

--- __main__.py
+++ __main__.py
@@ -352,7 +352,7 @@ if __name__ == '__main__':
         jmx_validity = get_jmxchecker_output(
             "~/installation_launchpad/apache-jmeter-5.1.1/bin/TestPlanCheck.sh --jmx " + ARGS.jmx)
 
-
+        print jmx_validity
 
         if "JMX is fine" not in jmx_validity:
             print "The Jmeter Script is not valid. Please correct it and Retry"
