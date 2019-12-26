5a42d5bfbbfc5d3420e1359601facfe4fd5e8adb
bpatra <badriprasad.patra@netapp.com>
2019-12-26 14:20:21 +0530

    Changed JSON to add Min, Max, Avg response time -6

--- lib/native.py
+++ lib/native.py
@@ -436,13 +436,10 @@ def setup(root_project_directory, input_map):
 
     print "-------------------References------------------------------------------"
     print "PerfoTron Dashboard                : " + perf_dashboard_url
-    print "\n"
     print "For all documentation              : " + home_url
-    print "\n"
     print "Convert your Jmeter Script         : " + convertor_url + "From (Browser)"
     print "                                   OR                                   "
     print "Convert and run your Jmeter Script :  use --jmx option from cli"
-    print "\n"
     print "-------------------References------------------------------------------"
 # ---------------------------Functions----------------------------------------------------
 
