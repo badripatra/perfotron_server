ddc3035c3bec741e635d8aa08ab6907fb73cab37
bpatra <badriprasad.patra@netapp.com>
2020-01-10 17:53:21 +0530

    Removed 8088 port entries from enable port and help content

--- os_specific_files/amazon-linux/enable_ports.sh
+++ os_specific_files/amazon-linux/enable_ports.sh
@@ -2,10 +2,7 @@ cd "$(dirname "$(realpath "$0")")";
 sudo fuser -k 3000/tcp
 sudo fuser -k 9003/tcp
 sudo fuser -k 8086/tcp
-sudo fuser -k 8088/tcp
 sudo systemctl enable firewalld
 sudo firewall-cmd --permanent --add-port=9003/tcp
-sudo firewall-cmd --permanent --add-port=8086/tcp
-sudo firewall-cmd --permanent --add-port=8088/tcp
 sudo firewall-cmd --permanent --add-port=3000/tcp
 sudo firewall-cmd --reload
