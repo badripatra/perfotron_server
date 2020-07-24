afeae6feb09a844a662139be6e22c3d5a044c3b3
Patra, Badri <Badriprasad.Patra@netapp.com>
2020-07-24 21:13:25 +0530

    Added reboot statement -2

--- demo_api/invoke_restapis.sh
+++ demo_api/invoke_restapis.sh
@@ -1,4 +1,4 @@
 cd "$(dirname "$(realpath "$0")")";
 kill -9 `ps -ef|grep -v grep|grep 'python demo_api.py'|awk '{print $2}'` > /dev/null 2>&1
 nohup python  demo_api.py > RestLogs.log &
-(crontab -l ; echo "@reboot nohup python ~/.installation_launchpad/demo_api.py > RestLogs.log &") 2>&1 | grep -v "no crontab" | sort | uniq | crontab -
+(crontab -l ; echo "@reboot nohup python `pwd`/demo_api.py > RestLogs.log &") 2>&1 | grep -v "no crontab" | sort | uniq | crontab -
